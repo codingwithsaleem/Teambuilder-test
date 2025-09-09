3c3d746adea08b3b8b20c28a25e91750e4ad64f4
saleem raza <sherohero33@gmail.com>
2025-09-09 11:21:57 +0500

    new chnages

--- comps/Home.tsx
+++ comps/Home.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BannerDataTypes, ProductsTypes } from "../app/page";
 import FooterBanner from "../comps/FooterBanner";
 import MainBanner from "./MainBanner";
@@ -11,30 +11,59 @@ interface HomeProps {
 }
 
 const Home = ({ products, bannerData }: HomeProps) => {
+  const [sortedProducts, setSortedProducts] = useState<ProductsTypes[]>(products);
+  const [sortOrder, setSortOrder] = useState<string>("default");
+
+  useEffect(() => {
+    handleSort(sortOrder);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sortOrder, products]);
+
+  const handleSort = (order: string) => {
+    let newSortedProducts = [...products];
+    
+    if (order === "lowToHigh") {
+      newSortedProducts.sort((a, b) => a.price - b.price);
+    } else if (order === "highToLow") {
+      newSortedProducts.sort((a, b) => b.price - a.price);
+    }
+    
+    setSortedProducts(newSortedProducts);
+  };
 
   return (
     <main>
       {/* === MAIN BANNER  */}
       <MainBanner banner={bannerData[0]} />
 
-      <section className="  mb-4 flex items-center flex-col">
+      <section className="mb-4 flex items-center flex-col">
         <h1
           className=" headTitle px-8 py-4 sm:py-2 sm:text-4xl text-2xl text-secondary
          font-sans font-extrabold sm:rounded-t-3xl"
         >
           Best Selling Headphones
         </h1>
-        {/* <p className=" text-base text-secondary">Best in the Market</p> */}
+        <div className="w-full max-w-xs mt-2">
+          <select 
+            className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-2 px-4 pr-8 rounded leading-tight focus:outline-none focus:border-primary"
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
+            <option value="default">Sort by price</option>
+            <option value="lowToHigh">Price: Low to High</option>
+            <option value="highToLow">Price: High to Low</option>
+          </select>
+        </div>
       </section>
 
       {/* === SHOW PRODUCTS  */}
       <section
-        className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4
+        className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3
        lg:mx-20 overflow-hidden
       "
       >
         {/* === MAP PRODUCTS  */}
-        {products?.map((products: ProductsTypes) => {
+        {sortedProducts?.map((products: ProductsTypes) => {
           return <Products key={products._id} products={products} />;
         })}
       </section>
